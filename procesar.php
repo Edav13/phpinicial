6d5ad41bc8d099a4f2c1b25fdbec08f8fe6cff27
Edav13 <Edav13@hotmail.com>
2016-06-18 21:47:52 -0400

    pequenia calculadora php

--- procesar.php
+++ procesar.php
@@ -1,9 +1,27 @@
-<?php 
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $pw = $_POST['pw'];
+<?php
+    if(isset($_POST['c1']) && !empty($_POST['c1']) &&isset($_POST['c2']) && !empty($_POST['c2']) && isset($_POST['c3']) && !empty($_POST['c3']) ){
 
-    echo $nombre."<br>";
-    echo $apellido."<br>";
-    echo $pw;
+        $operacion = $_POST['lista'];
+        echo "El resultado es: ";
+        switch($operacion)
+        {
+            case 'suma':
+            echo $_POST['c1'] + $_POST['c2'] + $_POST['c3'];
+            break;
+            case 'resta':
+            echo $_POST['c1'] - $_POST['c2'] - $_POST['c3'];
+            break;
+            case 'multiplicar':
+            echo $_POST['c1'] * $_POST['c2'] * $_POST['c3'];
+            break;
+            case 'dividir':
+            echo $_POST['c1'] / $_POST['c2'] / $_POST['c3'];
+            break;
+            default:
+            echo "No se realizo la operacion.";
+            break;
+        }
+    }else{
+        echo "Debes insertar todos los campos. ";
+    }
 ?>
