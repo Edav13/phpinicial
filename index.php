30592a5c6be712ae3ebaf55efc28298c34dc94a3
Edav13 <Edav13@hotmail.com>
2016-06-18 22:00:16 -0400

    crear archivos php

--- index.php
+++ index.php
@@ -8,20 +8,10 @@
 </head>
 <body>
 
-    <form action="procesar.php" method="post" accept-charset="utf-8" name="calc">
-        <input type="text" name="c1"><br>
-        <input type="text" name="c2"><br>
-        <input type="text" name="c3"><br>
-        
-        <label>Selecciona la operación: <br></label>
-        <select name="lista">
-            <option value="suma">Suma</option>
-            <option value="resta">Resta</option>
-            <option value="multiplicar">Multiplicar</option>
-            <option value="dividir">Dividir</option>
-        </select>
-        <br>
-        <input type="submit" value="Ver resultados">
+    <form action="procesar.php" method="post" accept-charset="utf-8" name="frm">
+        <input type="text" name="nombre"><br>
+        <textarea name="comentario"></textarea><br>
+        <input type="submit" value="Guardar datos">
     </form>
 </body>
 </html>
--- procesar.php
+++ procesar.php
@@ -1,27 +1,11 @@
 <?php
-    if(isset($_POST['c1']) && !empty($_POST['c1']) &&isset($_POST['c2']) && !empty($_POST['c2']) && isset($_POST['c3']) && !empty($_POST['c3']) ){
-
-        $operacion = $_POST['lista'];
-        echo "El resultado es: ";
-        switch($operacion)
-        {
-            case 'suma':
-            echo $_POST['c1'] + $_POST['c2'] + $_POST['c3'];
-            break;
-            case 'resta':
-            echo $_POST['c1'] - $_POST['c2'] - $_POST['c3'];
-            break;
-            case 'multiplicar':
-            echo $_POST['c1'] * $_POST['c2'] * $_POST['c3'];
-            break;
-            case 'dividir':
-            echo $_POST['c1'] / $_POST['c2'] / $_POST['c3'];
-            break;
-            default:
-            echo "No se realizo la operacion.";
-            break;
-        }
-    }else{
-        echo "Debes insertar todos los campos. ";
-    }
+    $fi = fopen("archivo.txt","a")
+    or die ("problemas al crear archivo");
+    fwrite($fi,"Datos: ");
+    fwrite($fi,"\n");
+    fwrite($fi,$_POST['nombre']."\n");
+    fwrite($fi,$_POST['comentario']."\n");
+    fwrite($fi,"-------------------------\n\n");
+    fclose($fi);
+    echo "Datos guardados.";
 ?>
